c367eb6105aa55ccb512ef12199fb2233ee12092
brisa jamiley jimenez castillo <bjimenez9@ucol.mx>
2019-08-23 17:49:27 -0500

    num

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Ciclos FOR");
+            Console.WriteLine("Coloca un numero");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Coloca un numero");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("de cuanto en cuanto:  ");
+            z = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                for (int i = x; i > y; i -= z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+
+            }
+            else
+            {
+                for (int i = x; i < y; i += z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+
+
+            }
+
+        }
+    }
+}
